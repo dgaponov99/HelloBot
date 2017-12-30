39062f72e48276e8b4ba04ac26b53e7596265aa4
dmitry <dgaponov99@mail.ru>
2017-12-30 18:31:54 +0300

    add sender

---
+++ sender.py
@@ -0,0 +1,16 @@
+import telebot
+import os
+
+import users_db
+from res import string_values
+
+bot = telebot.TeleBot(os.environ.get('TOKEN'))
+
+
+def send_hello():
+    users = users_db.get_users()
+    for user in users:
+        bot.send_message(user['user_id'], string_values.send_hello.format(user['first_name']))
+
+
+send_hello()
